781cd163fa8dc16869fd1376be6cd33de5a905cc
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2025-10-08 16:14:00 -0300

    Adicionando rotas no Axios (masmo as que não existem ainda)

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -20,8 +20,66 @@ api.interceptors.request.use(
 const sheets = {
   postLogin: (user) => api.post("login", user),
   postCadastro: (user) => api.post("user", user),
-  getProjects:()=> api.get("projects"),
-  searchProjects:(text)=>api.post("ROTA AINDA NÃO DESENVOLVIDA"),
+  getProjects: () => api.get("projects"),
+  searchProjects: (text) => api.post("ROTA AINDA NÃO DESENVOLVIDA", text),
+  filterProjects: (filter) => api.post("ROTA AINDA NÃO DESENVOLVIDA", filter),
+  getUserByName: (username) => api.get(`user/${username}`),
+  putUser: (user) => api.put(`user/${user.id}`, user),
+  deleteUser: (id) => api.delete(`user/${id}`),
+  updatePassword: (id, oldPassword, newPassword) =>
+    api.put(`user/newpassword/${id}`, {
+      senha_atual: oldPassword,
+      nova_senha: newPassword,
+    }),
+  getProjectsByUser: (username) => api.get(`projects/${username}`),
+  postProject: async (projeto, imageUri, idUser) => {
+    const data = new FormData();
+
+    for (let key in projeto) {
+      data.append(key, projeto[key]);
+    }
+
+    if (imageUri) {
+      const filename = imageUri.split("/").pop();
+      const match = /\.(\w+)$/.exec(filename);
+      const type = match ? `image/${match[1]}` : "image";
+      data.append("imagem", {
+        uri: imageUri,
+        name: filename,
+        type: type,
+      });
+    }
+
+    return api.post(`projects/${idUser}`, projeto, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  }, // TENHO QUE ESTUDAR COMO FUNCIONA O "ENVIAR MULTIPLAS IMAGENS PARA O SERVIDOR"
+  putProject: async (projeto, imageUri, id) => {
+    const data = new FormData();
+
+    for (let key in projeto) {
+      data.append(key, projeto[key]);
+    }
+
+    if (imageUri) {
+      const filename = imageUri.split("/").pop();
+      const match = /\.(\w+)$/.exec(filename);
+      const type = match ? `image/${match[1]}` : "image";
+      data.append("imagem", {
+        uri: imageUri,
+        name: filename,
+        type: type,
+      });
+    }
+
+    return api.put(`projects/${id}`, projeto, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  }, // TENHO QUE ESTUDAR COMO FUNCIONA O "ENVIAR MULTIPLAS IMAGENS PARA O SERVIDOR"
 };
 
 export default sheets;
