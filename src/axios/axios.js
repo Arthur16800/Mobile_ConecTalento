b36ceca9b1586a723a728fdb405407de8e8858a0
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2025-10-29 15:30:42 -0300

    removendo redundâncias

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,19 +1,6 @@
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
-function base64ToFile(base64, filename) {
-  if (!base64) return null;
-  const arr = base64.split(",");
-  const mime = arr[0].match(/:(.*?);/)[1];
-  const bstr = atob(arr[1]);
-  let n = bstr.length;
-  const u8arr = new Uint8Array(n);
-  while (n--) {
-    u8arr[n] = bstr.charCodeAt(n);
-  }
-  return new File([u8arr], filename, { type: mime });
-}
-
 const api = axios.create({
   baseURL: "http://10.89.240.88:5000/api/v1/",
   headers: { accept: "application/json" },
--- src/screens/CriarProjeto.js
+++ src/screens/CriarProjeto.js
@@ -10,7 +10,7 @@ import {
   Alert,
   ActivityIndicator,
 } from "react-native";
-import { useLayoutEffect, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
 import * as SecureStore from "expo-secure-store";
 import { SafeAreaView } from "react-native-safe-area-context";
